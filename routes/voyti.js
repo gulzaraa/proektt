afe4082ed4a1942c9e75d59d3835a26135856c8f
gulzara <ataberdiyevagulzara@gmail.com>
2022-10-27 21:28:35 +0300

    added routes

--- routes/voyti.js
+++ routes/voyti.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+let users = [];
  router.get('/', function(req, res, next) {
-     res.render('index');
+     res.render('voyti');
    });
   router.post("/", function(req, res, next) {
     console.log(req.body);
-    res.render("voyti", req.body);
+    users.push(req.body)
+    res.send(users)
     
   })
   module.exports = router;
