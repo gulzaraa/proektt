bc216ca6db2c03aa98dcc36aae2ced3eb664dce4
gulzara <ataberdiyevagulzara@gmail.com>
2022-10-27 23:36:40 +0300

    added html

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/about.ejs', function(req, res, next){
+
+router.get('/', function(req, res, next){
     res.render('about');
 });
 
