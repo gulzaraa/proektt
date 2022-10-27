d36a0f21eb8a601762b4bc9bfddf63cef1850d8d
Saparguly <115575521+Saparguly@users.noreply.github.com>
2022-10-28 00:08:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var costRouter = require('./routes/cost');
+var voytiRouter = require('./routes/voyti');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/cost', costRouter);
+app.use('/voyti', voytiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
